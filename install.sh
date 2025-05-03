989a60798b41425e91faba6236b0e2c3febf88af
neo <j6323622@gmail.com>
2025-05-03 01:13:23 +0000

    commit

--- install.sh
+++ install.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
 set -e  # 有錯誤就停止，避免意外覆蓋重要檔案
-
-echo "🔵 開始安裝 dotfiles symlink..."
-
-# Dotfiles 根目錄
-DOTFILES_DIR=$(pwd)
-
-# 目標是 HOME
-TARGET_DIR="$HOME"
 
 # symlink function（帶防呆提示）
 link_file() {
     local src=$1
     local dest=$2
 
-    if [ -e "$dest" ] || [ -L "$dest" ]; then
+    if [[ -e "$dest" ]] || [[ -L "$dest" ]]; then
         echo "⚠️  已存在：$dest，略過連結"
     else
         ln -s "$src" "$dest"
@@ -23,29 +18,30 @@ link_file() {
     fi
 }
 
+noBackup=("install.sh" "bak")
+
+# 檢查是否有備份
+if [[ ! -d ${DIR}/bak ]]; then
+    mkdir -p ${DIR}/bak
+    for i in $(ls ${DIR}); do
+        if [[ ${noBackup[*]} =~ ${i} ]]; then
+            continue
+        fi
+        cp -Lrp ${HOME}/."${i}" ${DIR}/bak/${i}
+    done
+else
+    echo no backup
+fi
+
+echo "🔵 開始安裝 dotfiles symlink..."
 # 建立基本的 symlink
-link_file "$DOTFILES_DIR/.bashrc" "$TARGET_DIR/.bashrc"
-link_file "$DOTFILES_DIR/.vimrc" "$TARGET_DIR/.vimrc"
-link_file "$DOTFILES_DIR/.tmux.conf" "$TARGET_DIR/.tmux.conf"
-link_file "$DOTFILES_DIR/.gitconfig" "$TARGET_DIR/.gitconfig"
-link_file "$DOTFILES_DIR/.gdbinit" "$TARGET_DIR/.gdbinit"
 
-# Vim 資料夾（plugin / 配色）
-if [ -d "$DOTFILES_DIR/.vim" ]; then
-    link_file "$DOTFILES_DIR/.vim" "$TARGET_DIR/.vim"
-fi
-
-# .local/bin（自定小工具）
-if [ -d "$DOTFILES_DIR/.local/bin" ]; then
-    mkdir -p "$TARGET_DIR/.local"
-    link_file "$DOTFILES_DIR/.local/bin" "$TARGET_DIR/.local/bin"
-fi
-
-# .config/coc（vim LSP client設定）
-if [ -d "$DOTFILES_DIR/.config/coc" ]; then
-    mkdir -p "$TARGET_DIR/.config"
-    link_file "$DOTFILES_DIR/.config/coc" "$TARGET_DIR/.config/coc"
-fi
+for i in $(ls ${DIR}); do
+    if [[ ${noBackup[*]} =~ ${i} ]]; then
+        continue
+    fi
+    link_file "${HOME}/.${i}" "${DIR}/${i}"
+done
 
 echo "🏁 安裝完成！記得 source ~/.bashrc 或重新登入。"
 
